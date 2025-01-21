5b42d4d4df5716d6cb5c419f7e23d79d6d2cdbf1
gane2e <b1a409896@naver.com>
2025-01-21 17:51:06 +0900

    [가은] SQL주석제거

--- src/main/java/com/lms/repository/SubCategoryRepository.java
+++ src/main/java/com/lms/repository/SubCategoryRepository.java
@@ -11,14 +11,6 @@ import java.util.List;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
 
-    /*
-//    /* JPQL문법 */
-//    @Query("select o from Order o " +
-//            "where o.member.email = :email " +
-//            "order by o.orderDate desc")
-//    List<Order> findOrders(@Param("email") String email, Pageable pageable);
-//    /* 현재 로그인한 사용자의 주문 데이터를 페이징 조건에 맞춰서 조회 */*/
-
     @Query("SELECT s " +
             "FROM SubCategory s " +
             "WHERE s.categories.categoryId = :categoryId")
