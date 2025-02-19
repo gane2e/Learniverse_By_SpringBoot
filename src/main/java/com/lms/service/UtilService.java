ea676ad54a46ad5b916418a812c16f727058e3ef
gane2e <b1a409896@naver.com>
2025-02-19 14:17:36 +0900

    [가은] username 공통처리 UtilService 추가

--- src/main/java/com/lms/service/UtilService.java
+++ src/main/java/com/lms/service/UtilService.java
@@ -1,4 +1,27 @@
 package com.lms.service;
 
+import com.lms.entity.Member;
+import com.lms.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Log4j2
 public class UtilService {
+
+    MemberRepository memberRepository;
+
+    public String getUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Member member = memberRepository.findByLoginId(username);
+        String name = member.getName();
+        return name;
+    }
 }
