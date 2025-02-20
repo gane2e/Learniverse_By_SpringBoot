41e07471766539a160d3b32beb0f123877d834ee
gane2e <b1a409896@naver.com>
2025-02-21 08:33:29 +0900

    [가은] 로그인/비로그인 체크메서드 추가

--- src/main/java/com/lms/util/UserCheck.java
+++ src/main/java/com/lms/util/UserCheck.java
@@ -1,4 +1,21 @@
 package com.lms.util;
 
-public class loginCheck {
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserCheck {
+
+    public static boolean isUserLoggedIn() {
+
+        Authentication authentication
+                = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated())
+            return true; //로그인
+        else
+            return false; //비로그인
+    }
+
 }
