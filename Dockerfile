d5a737b90231c99ad22a01e0f4aebd89ec8c0fcb
gane2e <b1a409896@naver.com>
2025-02-21 17:40:35 +0900

    [가은] 도커 환경세팅

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,15 @@
-# DOCKER FILE
+# 1. OpenJDK 17 기반 이미지 사용
 FROM openjdk:17-jdk
-LABEL mainteiner=""
+
+# 2. 컨테이너에 볼륨 설정
+VOLUME /lms
+
+# 3. JAR 파일 경로 설정
+ARG JAR_FILE=build/libs/lmsSite.jar
+
+# 4. JAR 파일을 컨테이너로 복사
+COPY ${JAR_FILE} app.jar
+
+# 5. 애플리케이션 실행 명령어 설정
+ENTRYPOINT ["java", "-jar", "app.jar"]
+
---
+++ init.sql
@@ -0,0 +1,3 @@
+insert into member(member_id, reg_time, update_time, modified_by, address, birthdate, email, gender, login_id, mobile_number, name, password)
+values(1, localtimestamp(), localtimestamp(), 'user', 'address', localtimestamp(), 'admin001@admin.com', '남성', 'admin_001', '01012345678', 'admin', '$2a$10$X2l8Exm3nBqx5xlgJzOrWu0RGqGQ3Z7F8NHF8OHU0OIZD4TYuVuPS
+');
