6c76a12a011686bde5b6cc612052a33239f4e6d9
gane2e <b1a409896@naver.com>
2025-02-21 17:41:23 +0900

    [가은] isUserLoggedin 메서드 수정

--- src/main/java/com/lms/util/UserCheck.java
+++ src/main/java/com/lms/util/UserCheck.java
@@ -1,5 +1,6 @@
 package com.lms.util;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -7,15 +8,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserCheck {
 
-    public static boolean isUserLoggedIn() {
-
-        Authentication authentication
-                = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication != null && authentication.isAuthenticated())
-            return true; //로그인
-        else
-            return false; //비로그인
+    //로그인, 비로그인 체크용 메서드
+    public static boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 
 }
